158e04f7d1973331b033d0e44fc6683e601eb00f
Vika94 <73698615+Vika94@users.noreply.github.com>
2021-08-23 15:09:51 +0300

    Update Task6.java

--- src/test/java/Task6.java
+++ src/test/java/Task6.java
@@ -18,7 +18,7 @@ public class Task6 {
 
     @Test
     public void test1() {
-         // driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        // driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.name("password")).sendKeys("secret_sauce");
         driver.findElement(By.className("submit-button")).click();
