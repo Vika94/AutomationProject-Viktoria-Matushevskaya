49f20e23dc128b4c7bddbe29ca61b2b0bce6abb4
Vika94 <73698615+Vika94@users.noreply.github.com>
2021-09-26 15:40:45 +0300

    added Task15

---
+++ src/test/java/Task15/listResource/Datum1.java
@@ -0,0 +1,12 @@
+package Task15.listResource;
+
+import lombok.Data;
+
+@Data
+public class Datum1 {
+    public int id;
+    public String name;
+    public int year;
+    public String color;
+    public String pantone_value;
+}
