7a12c79180e0a1bc6cb92d40640db0dc115f2898
Vika94 <73698615+Vika94@users.noreply.github.com>
2021-10-09 16:34:35 +0300

    added Task17

--- src/test/java/Task17/Task17.java
+++ src/test/java/Task17/Task17.java
@@ -12,50 +12,50 @@ public class Task17 {
 
     @Description("Select inquiry")
     @Test
-    public void test1() throws Exception {
+    public void test1() {
         getSelect().select("*").from("user").where("id = 0").getListData().forEach(System.out::println);
     }
 
     @Test
-    public void test2() throws Exception {
+    public void test2() {
         getSelect().select("*").from("user").where("age = '32' and first_name = 'User 2'").getListData().forEach(System.out::println);
     }
 
     @Description("Insert inquiry")
     @Test
-    public void test3() throws Exception {
+    public void test3() {
         getInsert().insert().into("user (id, first_name, last_name, age)").values("('11', 'Vika', 'Added', '23')").resultSetInsert();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
 
     @Test
-    public void test4() throws Exception {
+    public void test4() {
         getInsert().insert().into("user (id, first_name, last_name, age)").values("('12', 'Cat', '', '2')").resultSetInsert();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
 
     @Description("Update inquiry")
     @Test
-    public void test5() throws Exception {
+    public void test5() {
         getUpdate().update("user").set("id = 55").where("id = 11").resultSetUpdate();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
 
     @Test
-    public void test6() throws Exception {
+    public void test6() {
         getUpdate().update("user").set("first_name = 'Dog'").where("first_name = 'Cat'").resultSetUpdate();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
 
     @Description("Delete inquiry")
     @Test
-    public void test7() throws Exception {
+    public void test7() {
         getDelete().delete().from("user").where("id = 12").resultSetDelete();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
 
     @Test
-    public void test8() throws Exception {
+    public void test8() {
         getDelete().delete().from("user").where("first_name = 'Vika'").resultSetDelete();
         getSelect().select("*").from("user").getListData().forEach(System.out::println);
     }
